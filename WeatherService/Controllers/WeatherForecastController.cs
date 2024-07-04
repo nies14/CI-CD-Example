abe73fb50ebb7420bf53cab0f2492543807b5315
nies14 <mthnies@gmail.com>
2024-07-04 16:41:09 -0400

    Added comment

--- WeatherService/Controllers/WeatherForecastController.cs
+++ WeatherService/Controllers/WeatherForecastController.cs
@@ -6,6 +6,7 @@ namespace WeatherService.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
+    // Summaries
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
